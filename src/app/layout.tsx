b8248996746e47bdab0e39f3335e66fcfbe97961
Dirty Martini <hello@maxwellkirwin.co.uk>
2024-09-12 16:39:12 +0100

    33:41

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={alpino.variable}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
